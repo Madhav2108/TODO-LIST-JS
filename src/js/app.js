faba119bad521aa21bf3a233a572e595dba7643f
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-08 04:46:44 +0530

    current date connected

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
+
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; 
+    loadList(LIST); 
+}else{
+    LIST = [];
+    id = 0;
+}
+
+
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
+// Show todays date
 const options = {weekday : "long", month:"short", day:"numeric"};
 const today = new Date();
 
