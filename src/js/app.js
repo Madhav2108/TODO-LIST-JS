62b74cfb38d303ff4a516dca7f1a7757148b16f3
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-11 01:49:34 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -82,3 +82,11 @@ document.addEventListener("keyup",function(even){
         input.value = "";
     }
 });
+
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+    
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
